3ccee3c171a5092fc6b20dc0fd41333f8662a169
danh9a99 <57696188+danh9a99@users.noreply.github.com>
2020-03-14 09:55:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,60 @@
-const APP_SECRET = '71039e1375a8646c665426c95058eed2';
-const VALIDATION_TOKEN = 'TokenTuyChon';
-const PAGE_ACCESS_TOKEN = 'EAAgeOMAwZCzEBAHwAR0c8iP70ZBGrMWwUfT47SIyZCwygnnZBCqtSqnQQ5ukqXZCEkotUq4riaB3WMb2aH6EF3ZC5HQCgbcg7AmGyuFgS9knAZABvp6vK2ZBINoTDRxrE3vCaZBj5CZCutgYFMHA4y6gKZASXUbwDMAPhZCakEPkDT2CvQZDZD';
- 
+// # SimpleServer
+// A simple chat bot server
+var logger = require('morgan');
 var http = require('http');
 var bodyParser = require('body-parser');
 var express = require('express');
- 
+var router = express();
+
 var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 var server = http.createServer(app);
 var request = require("request");
- 
+
 app.get('/', (req, res) => {
-  res.send("Server chay ngon lanh !!");
+  res.send("Home page. Server running okay.");
 });
- 
-app.get('/webhook', function(req, res) { // Đây là path để validate tooken bên app facebook gửi qua
-  if (req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+
+// Đây là đoạn code để tạo Webhook
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === 'bapp_token') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
 });
- 
-app.post('/webhook', function(req, res) { // Phần sử lý tin nhắn của người dùng gửi đến
+
+// Xử lý khi có người nhắn tin cho bot
+app.post('/webhook', function(req, res) {
   var entries = req.body.entry;
   for (var entry of entries) {
     var messaging = entry.messaging;
     for (var message of messaging) {
       var senderId = message.sender.id;
       if (message.message) {
+        // If user send text
         if (message.message.text) {
           var text = message.message.text;
-          sendMessage(senderId, "Hello!! I'm a bot. Your message: " + text);
+          console.log(text); // In tin nhắn người dùng
+          sendMessage(senderId, "Tui là bot đây: " + text);
         }
       }
     }
   }
+
   res.status(200).send("OK");
 });
- 
-// Đây là function dùng api của facebook để gửi tin nhắn
+
+
+// Gửi thông tin tới REST API để trả lời
 function sendMessage(senderId, message) {
   request({
     url: 'https://graph.facebook.com/v6.0/me/messages',
     qs: {
-      access_token: PAGE_ACCESS_TOKEN,
+      access_token: "token",
     },
     method: 'POST',
     json: {
@@ -59,10 +67,10 @@ function sendMessage(senderId, message) {
     }
   });
 }
- 
-app.set('port', process.env.PORT || 5000);
-app.set('ip', process.env.IP || "0.0.0.0");
- 
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+
 server.listen(app.get('port'), app.get('ip'), function() {
   console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
 });
